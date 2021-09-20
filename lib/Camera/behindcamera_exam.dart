527bf572e48aabfca50511c174aeacf16738eab2
maropook <ii440092@icloud.com>
2021-09-20 09:26:18 +0900

    ループを無くした

--- lib/Camera/behindcamera_exam.dart
+++ lib/Camera/behindcamera_exam.dart
@@ -1,3 +1,6 @@
+// 9/20
+// カメラ機能のみで，動画を録画し，その動画と字幕ファイルを合成する．
+
 import 'dart:async';
 import 'dart:io';
 import 'package:ffmpeg_flutter_test/ffmpeg/flutter_ffmpeg_api_wrapper.dart';
@@ -8,18 +11,7 @@ import 'package:path/path.dart' as p;
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_recognition_error.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:video_player/video_player.dart';
-
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-//非同期処理用ライブラリ
-import 'dart:async';
-//アプリがファイルを保存可能な場所を取得するライブラリ
-import 'package:path_provider/path_provider.dart';
-
-//出力するテキストファイル名
-final _fileName = 'editTextField.txt';
 
 class CameraExampleHome extends StatefulWidget {
   @override
@@ -75,132 +67,9 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
   // Counting pointers (number of user fingers on screen)
   int _pointers = 0;
 
-  String _out = '';
-  String lastWords = "　";
-  String lastError = '';
-  String lastStatus = '';
-  stt.SpeechToText speech = stt.SpeechToText();
-
-  bool flug = false;
-  late Timer _timer;
-  late int _currentSeconds;
-
-  int strcount = 0;
-
-  Timer countTimer() {
-    return Timer.periodic(
-      const Duration(seconds: 1),
-      (Timer timer) {
-        if (!flug) {
-          timer.cancel();
-        } else {
-          setState(() {
-            _currentSeconds = _currentSeconds + 1;
-            strcount = strcount + 1;
-
-            getFilePath().then((File file) {
-              file.writeAsString(
-                '${strcount}\n${timerString(_currentSeconds)} --> ${timerString(_currentSeconds + 1)}\n${lastWords}\n\n',
-                mode: FileMode.append,
-              );
-            });
-          });
-        }
-      },
-    );
-  }
-
-  Future<void> _speak() async {
-    getFilePath().then((File file) {
-      file.writeAsString("");
-      //ファイルの初期化
-    });
-
-    bool available = await speech.initialize(
-        onError: errorListener, onStatus: statusListener);
-    if (available) {
-      flug = true;
-      if (!_timer.isActive) {
-        setState(() {
-          _timer = countTimer();
-        });
-      }
-
-      speech.listen(
-        onResult: resultListener,
-        localeId: "Japanese",
-      );
-    } else {
-      print("The user has denied the use of speech recognition.");
-    }
-  }
-
-  //ファイルの出力処理
-
-  //ファイルの読み込みと変数への格納処理
-  void loadButton() async {
-    setState(() {
-      load().then((String value) {
-        setState(() {
-          _out = value;
-          print(_out);
-        });
-      });
-    });
-  }
-
-  Future<void> _stop() async {
-    loadButton();
-
-    //タイマーを止める
-    if (_timer.isActive) {
-      _timer.cancel();
-    }
-
-    speech.stop();
-  }
-
-  void resultListener(SpeechRecognitionResult result) {
-    setState(() {
-      lastWords = '${result.recognizedWords}';
-    });
-  }
-
-  void errorListener(SpeechRecognitionError error) {
-    setState(() {
-      lastError = '${error.errorMsg} - ${error.permanent}';
-    });
-  }
-
-  void statusListener(String status) {
-    setState(() {
-      lastStatus = '$status';
-      print(lastStatus);
-    });
-  }
-
-  Widget _timeStr() {
-    return Text(
-      timerString(_currentSeconds),
-      // style: TextStyle(fontSize: 32, color: Colors.black),
-    );
-  }
-
-  String timerString(int leftSeconds) {
-    // 00:00:06,000
-    final minutes = (leftSeconds / 60).floor().toString().padLeft(2, '0');
-    final seconds = (leftSeconds % 60).floor().toString().padLeft(2, '0');
-    return '00:$minutes:${seconds},000';
-  }
-
   @override
   void initState() {
     super.initState();
-    final workMinuts = 0;
-    _currentSeconds = workMinuts * 60;
-
-    _timer = countTimer();
-
     _ambiguate(WidgetsBinding.instance)?.addObserver(this);
 
     _flashModeControlRowAnimationController = AnimationController(
@@ -235,7 +104,6 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
     _flashModeControlRowAnimationController.dispose();
     _exposureModeControlRowAnimationController.dispose();
     super.dispose();
-    _timer.cancel();
   }
 
   @override
@@ -296,37 +164,15 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
               ],
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: [
-                  IconButton(icon: Icon(Icons.add), onPressed: loadButton),
-                  _timeStr(),
-                  Text(
-                    ' ステータス : ${lastStatus}',
-                  ),
-                ],
-              ),
-              Text(
-                ' 変換文字:${lastWords}',
-              ),
-            ],
-          ),
         ],
       ),
-      floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-        FloatingActionButton(onPressed: _speak, child: Icon(Icons.play_arrow)),
-        FloatingActionButton(onPressed: _stop, child: Icon(Icons.stop)),
-        FloatingActionButton(onPressed: () {
-          print(videoFile!.path);
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MainffmpegPage(videoFile!.path)));
-        }),
-      ]),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        print(videoFile!.path);
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MainffmpegPage(videoFile!.path)));
+      }),
     );
   }
 
@@ -580,17 +426,41 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
   }
 
   void onVideoRecordButtonPressed() {
-    //start video record
     startVideoRecording().then((_) {
       if (mounted) setState(() {});
     });
   }
 
   void onStopButtonPressed() {
-    //stop video record
     stopVideoRecording().then((file) {
       if (mounted) setState(() {});
       if (file != null) {
+        // String burnSubtitlesCommand =
+        //     "-y -i ${"/Users/hasegawaitsuki/ghq/github.com/maropook/ffmpeg_flutter_test/assets/a.mp4"} -vf scale=1920:1080,pad=1920:1080:0:0:black -c:v mpeg4 ${"/Users/hasegawaitsuki/ghq/github.com/maropook/ffmpeg_flutter_test/assets/camera.mp4"} ";
+        // // どのvideoと合成するか．
+        // //  "-y -i ${videoFile.path} -vf subtitles=$subtitlePath:force_style='Fontname=Trueno' -c:v mpeg4 ${videoWithSubtitlesFile.path}";
+
+        // executeAsyncFFmpeg(burnSubtitlesCommand,
+        //     (CompletedFFmpegExecution secondExecution) {
+        //   ffprint(
+        //       "FFmpeg process exited with rc ${secondExecution.returnCode}.");
+
+        //   if (secondExecution.returnCode == 0) {
+        //     ffprint("Burn subtitles completed successfully; playing video.");
+        //   } else if (secondExecution.returnCode == 255) {
+        //     showPopup("Burn subtitles operation cancelled.");
+        //     ffprint("Burn subtitles operation cancelled");
+        //   } else {
+        //     showPopup(
+        //         "Burn subtitles failed. Please check log for the details.");
+        //     ffprint(
+        //         "Burn subtitles failed with rc=${secondExecution.returnCode}.");
+        //   }
+        // }).then((executionId) {
+        //   ffprint(
+        //       "Async FFmpeg process started with arguments '$burnSubtitlesCommand' and executionId $executionId.");
+        // });
+
         showInSnackBar('saved Video recorded to ${file.path}');
         videoFile = file;
         _startVideoPlayer();
@@ -664,6 +534,14 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
     await vController.play();
   }
 
+  Future<String> returnVideoPath() async {
+    if (videoFile == null) {
+      return '/Users/hasegawaitsuki/ghq/github.com/maropook/ffmpeg_flutter_test/assets/video.mp4';
+    }
+
+    return videoFile!.path;
+  }
+
   void _showCameraException(CameraException e) {
     logError(e.code, e.description);
     showInSnackBar('Error: ${e.code}\n${e.description}');
@@ -699,17 +577,3 @@ Future<void> main() async {
 /// with `!` and `?` on the stable branch.
 // TODO(ianh): Remove this once we roll stable in late 2021.
 T? _ambiguate<T>(T? value) => value;
-
-Future<File> getFilePath() async {
-  final directory = await getTemporaryDirectory();
-
-  return File(directory.path + '/' + _fileName);
-  // return File(
-  //     '/Users/hasegawaitsuki/ghq/github.com/maropook/ffmpeg_flutter_test/assets/subtitle.srt');
-}
-
-//テキストファイルの読み込み
-Future<String> load() async {
-  final file = await getFilePath();
-  return file.readAsString();
-}
