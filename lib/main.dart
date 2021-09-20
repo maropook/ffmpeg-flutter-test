ab3463773b643435df20087a92457795f887591a
maropook <ii440092@icloud.com>
2021-09-20 18:39:48 +0900

    カメラ機能をホームにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,6 @@
-import 'dart:io';
 import 'package:camera/camera.dart';
-
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'Camera/camera_example_home.dart';
-import 'ffmpeg/ui.dart';
-
-GlobalKey _globalKey = GlobalKey();
-
-class CounterStorage {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<String> get _localPathtest async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File(
-        '/Users/hasegawaitsuki/ghq/github.com/maropook/ffmpeg_flutter_test/assets/counter.txt');
-    //return File('$path/counter.txt');
-    // =/Users/hasegawaitsuki/Library/Developer/CoreSimulator/Devices/661DC9A5-9224-4BC9-A812-223BA1DD0FD9/data/Containers/Data/Application/C0C0BB6D-8894-403D-A200-24932A938ADD/Documents/counter.txt'
-  }
-
-  Future<File> get _localFiletest async {
-    final path = await _localPath;
-    return File(
-        '/Users/hasegawaitsuki/ghq/github.com/maropook/ffmpeg_flutter_test/assets/test.txt');
-    //return File('$path/counter.txt');
-    // =/Users/hasegawaitsuki/Library/Developer/CoreSimulator/Devices/661DC9A5-9224-4BC9-A812-223BA1DD0FD9/data/Containers/Data/Application/C0C0BB6D-8894-403D-A200-24932A938ADD/Documents/counter.txt'
-  }
-
-  Future<int> readCounter() async {
-    try {
-      final file = await _localFile;
-
-      // Read the file
-      String contents = await file.readAsString();
-
-      return int.parse(contents);
-    } catch (e) {
-      // If encountering an error, return 0
-      return 0;
-    }
-  }
-
-  Future<File> writeCounter(int counter) async {
-    final file = await _localFile;
-    final filetest = await _localFiletest;
-
-    filetest.writeAsString('$counter\n\n', mode: FileMode.append, flush: true);
-
-    // Write the file
-    return file.writeAsString('$counter');
-  }
-}
 
 Future<void> main() async {
   // Fetch the available cameras before initializing the app.
@@ -77,41 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '色々',
+      title: 'neon',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Top(),
-    );
-  }
-}
-
-class Top extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('一覧'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 8),
-            ElevatedButton(
-              child: Text(
-                  '【公式サンプル】カメラ，動画撮影,/camera,video_player'), //写真も動画も撮れる．実機で動かさないといけない.cameraの公式のただのsample
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => CameraExampleHome()));
-              },
-            ),
-          ],
-        ),
-      ),
+      home: CameraExampleHome(),
     );
   }
 }
